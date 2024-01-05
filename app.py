d7b2a6ba49ce8aaa9561e619a00df71eeadcc0ce
YuQiao0428 <91924051+YuQiao0428@users.noreply.github.com>
2024-01-05 16:32:56 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+#載入LineBot所需要的套件
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+import re
+app = Flask(__name__)
+
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi('RDvRNUU9sCtMhKdaLvNbaQOCeRZ/G4b5HIKp15lvpU0VeCEPPucCOHefd133hfRfGgxm63+Y32cgP9cllest9otdTU6Q7WE48UMkd5EDSMNWdWkdLyq/YEu7yhB3h6Hq8S8yb0FYmM3GQ5+0dtaV/wdB04t89/1O/w1cDnyilFU=')
+# 必須放上自己的Channel Secret
+handler = WebhookHandler('eb2de85826d9dac8f365ebfc9b5e353c')
+
+line_bot_api.push_message('Ud23a818c00847246a190a4f396ea6ebb', TextSendMessage(text='你可以開始了'))
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        # 貼圖查詢 : https://developer.line.biz/en/docs/messaging-api/sticker-list/#specify-sticker-in-message-object
+        sticker_message = StickerSendMessage(
+                package_id = '1',
+                sticker_id = '1' 
+        )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+#主程式
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
